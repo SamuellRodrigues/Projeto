0bd4e8e80f200265c1e43d3d769fc25ce8e0a018
SamuellRodrigues <baurukigamer@gmail.com>
2025-07-01 14:31:34 -0300

    atualizaçao das cores

--- TarefaApp/GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
+++ TarefaApp/GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf829266474a8d9d23f01a0c7a149d53f9e65c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2873b3137592801b7a4b603e1371a8d43951c012")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
